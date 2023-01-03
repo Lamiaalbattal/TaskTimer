d46a3700e2b44e42251b16975771ae2258e6ccca
Lamia AlDossari <lamia1albattal@gmail.com>
2023-01-03 03:09:25 +0300

    Initial Commit

---
+++ TaskTimer/TaskTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTimerApp.swift
+//  TaskTimer
+//
+//  Created by Lamia Aldossari on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
